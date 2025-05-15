6f192373439d39801ac88f92ab18c401961c3849
webdew1001 <webdew010@gmail.com>
2025-05-15 17:23:08 +0900

    평면도 추가

--- js/main.js
+++ js/main.js
@@ -1,108 +1,112 @@
-window.onload = () => { 
-    let body = $('body');
-    let btnDown = $('.check-radio .btn-down');
-    let infoGuide = $('.check-radio .guide-01');
-    let check = $('.check-radio .form-checked'); 
-    let btnSubmitTop = $('.btn-submit-top');
-    let btnSubmitBottom = $('.btn-submit-bottom');
+window.onload = () => {
+  let body = $("body");
+  let btnDown = $(".check-radio .btn-down");
+  let infoGuide = $(".check-radio .guide-01");
+  let check = $(".check-radio .form-checked");
+  let btnSubmitTop = $(".btn-submit-top");
+  let btnSubmitBottom = $(".btn-submit-bottom");
 
-    
-    btnDown.click(function(){
-        $(this).toggleClass('active');
-        console.log($(this))
-        $(this).parent().next(infoGuide).slideToggle();
-    });
+  btnDown.click(function () {
+    $(this).toggleClass("active");
+    console.log($(this));
+    $(this).parent().next(infoGuide).slideToggle();
+  });
 
-     console.log($('.use-checkbox-top').length)
-    $('.use-checkbox-top').change(function(){
-        if($('.use-checkbox-top:checked').length === $('.use-checkbox-top').length) {
-            btnSubmitTop.addClass('active')
-        } else {
-            btnSubmitTop.removeClass('active')
-        }
-    })
-    $('.use-checkbox-bottom').change(function(){
-        if($('.use-checkbox-bottom:checked').length === $('.use-checkbox-bottom').length) {
-            btnSubmitBottom.addClass('active')
-        } else {
-            btnSubmitBottom.removeClass('active')
-        }
-    })
+  console.log($(".use-checkbox-top").length);
+  $(".use-checkbox-top").change(function () {
+    if (
+      $(".use-checkbox-top:checked").length === $(".use-checkbox-top").length
+    ) {
+      btnSubmitTop.addClass("active");
+    } else {
+      btnSubmitTop.removeClass("active");
+    }
+  });
+  $(".use-checkbox-bottom").change(function () {
+    if (
+      $(".use-checkbox-bottom:checked").length ===
+      $(".use-checkbox-bottom").length
+    ) {
+      btnSubmitBottom.addClass("active");
+    } else {
+      btnSubmitBottom.removeClass("active");
+    }
+  });
 
-     
+  var swiper = new Swiper(".locationSwiper", {
+    slidesPerView: 1.2,
+    spaceBetween: 16,
+    breakpoints: {
+      641: {
+        slidesPerView: 2.4,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 2.2,
+        spaceBetween: 40,
+      },
+    },
 
-    var swiper = new Swiper(".locationSwiper", {
-        slidesPerView: 1.2,
-        spaceBetween: 16,
-        breakpoints: {
-            641: {
-                slidesPerView: 2.4,
-                spaceBetween: 30,
-            },
-            1200: {
-                slidesPerView: 2.2,
-                spaceBetween: 40,
-            },
-        }
-         
-        // centeredSlides: true,
-    });
+    // centeredSlides: true,
+  });
 
-    let header = $('.header');
-    let headerTop = $('.header').offset().top;
-    let headerLogo = $('.logo img');
-    let mainVisualTop = $('.form_wrap').offset().top;
-    let locationTop = $('.location').offset().top;
-    let introTop = $('.intro').offset().top;
+  let header = $(".header");
+  let headerTop = $(".header").offset().top;
+  let headerLogo = $(".logo img");
+  let mainVisualTop = $(".form_wrap").offset().top;
+  let locationTop = $(".location").offset().top;
+  let introTop = $(".intro").offset().top;
 
-    
-    let sideFormBtn = $('.side_fixed_form');
-    let sideTopBtn = $('.side_fixed_top');
-    console.log(headerTop)
-    $(window).scroll(function(){
-        let scrollBar = $(window).scrollTop();
-        
-        if(scrollBar > 0) {
-            header.addClass('active');
-            headerLogo.attr('src', 'img/logo_scroll.png')
-        } else {
-            headerLogo.attr('src', 'img/logo.png')
-            header.removeClass('active');
-        }
+  let sideFormBtn = $(".side_fixed_form");
+  let sideTopBtn = $(".side_fixed_top");
+  console.log(headerTop);
+  $(window).scroll(function () {
+    let scrollBar = $(window).scrollTop();
 
-        if(scrollBar >= locationTop - 500 && scrollBar <= introTop - 500) {
-            sideFormBtn.addClass('active');
-        } else {
-            sideFormBtn.removeClass('active');
-        }
+    if (scrollBar > 0) {
+      header.addClass("active");
+      headerLogo.attr("src", "img/logo_scroll.png");
+    } else {
+      headerLogo.attr("src", "img/logo.png");
+      header.removeClass("active");
+    }
 
-        if(scrollBar >= locationTop - 500) {
-            sideTopBtn.addClass('active');
-        } else {
-            sideTopBtn.removeClass('active');
-        }
+    if (scrollBar >= locationTop - 500 && scrollBar <= introTop - 500) {
+      sideFormBtn.addClass("active");
+    } else {
+      sideFormBtn.removeClass("active");
+    }
 
-    });
+    if (scrollBar >= locationTop - 500) {
+      sideTopBtn.addClass("active");
+    } else {
+      sideTopBtn.removeClass("active");
+    }
+  });
 
-    let modal = $('.modal');
-    let modalCloase = $('.modal .modal_close');
-    let sideFixedPopup = $('.side_fixed_popup');
-    let sidePopupImage = $('.side_popup');
+  let modal = $(".modal");
+  let modalCloase = $(".modal .modal_close");
+  let sideFixedPopup = $(".side_fixed_popup");
+  let sidePopupImage = $(".side_popup");
 
-    sideFixedPopup.click(function(){
-        sidePopupImage.slideToggle();
-    })
-    modalCloase.click(function(){
-        body.removeClass('scroll');
-        modal.addClass('active');
-    });
+  sideFixedPopup.click(function () {
+    sidePopupImage.slideToggle();
+  });
+  modalCloase.click(function () {
+    body.removeClass("scroll");
+    modal.addClass("active");
+  });
 
-    
-      /*   if($(window).width() < 481) {
+  /*   if($(window).width() < 481) {
              
             //alert()
             modal.addClass('mobile');
         }  */
-    
-    
-}
+
+  // 2025.05.15
+  var swiper = new Swiper(".floorSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+};
